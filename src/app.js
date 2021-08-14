4b2d49100d204cc9f45b62dc2d658a5ee27b4dfd
tepeszCodes <tomek.szymanderski@gmail.com>
2021-08-14 19:10:07 +0200

    fixed warnings and errors thrown by eslint

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
+
 const app = express();
 
-app.get('/', (req,res) => {
-  res.send("This is my first Express server");
+app.get('/', (req, res) => {
+  res.send('This is my first Express server');
 });
 
 app.listen(process.env.port || 3000);
-console.log('Web Server is listening at port '+ (process.env.port || 3000));
