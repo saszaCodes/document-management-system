9d5c4c8da7402021cd063aeb58896b51fc1c55ee
tepeszCodes <tomek.szymanderski@gmail.com>
2021-08-17 20:26:17 +0200

    added app.test.js and updated app.js

--- src/app.js
+++ src/app.js
@@ -11,3 +11,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+export default app;
---
+++ tests/app.test.js
@@ -0,0 +1,7 @@
+import app from '../src/app';
+import supertest from 'supertest';
+import { expect, test } from 'jest';
+
+test('expect it to work', () => {
+  expect(() => 2 + 2).toBe(4);
+});
