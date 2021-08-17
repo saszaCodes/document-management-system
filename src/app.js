f3ef7d5968d1206a694b200a472cbf60661cf569
tepeszCodes <tomek.szymanderski@gmail.com>
2021-08-17 11:27:30 +0200

    added error handler middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import errorHandler from './errorHandler';
 
 const app = express();
+const PORT = process.env.port || 3000;
+const { genericError } = errorHandler;
 
 app.get('/', (req, res) => {
   res.send('This is my first Express server');
 });
 
-app.listen(process.env.port || 3000);
+app.use('/', (err, req, res, next) => {
+  // if response has already started streaming and error occurs, pass it to Express
+  // default error handler - it will close the connection and fail the request.
+  if (res.headersSent) {
+    next(err);
+  } else {
+    genericError(err, req, res);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
