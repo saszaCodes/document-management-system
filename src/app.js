6582fb15cb96625c92da1b71c27bf4da1f01cc75
tepeszCodes <54599709+tepeszCodes@users.noreply.github.com>
2021-08-17 09:58:32 +0200

    Implemented suggested change
    Co-authored-by: Audax Anchirinah <audax338@gmail.com>

--- src/app.js
+++ src/app.js
@@ -2,8 +2,12 @@ import express from 'express';
 
 const app = express();
 
+const PORT = process.env.port || 3000;
+
 app.get('/', (req, res) => {
   res.send('This is my first Express server');
 });
 
-app.listen(process.env.port || 3000);
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT);
+});
