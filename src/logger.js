aaac5c99100cf29fc2588e3b33e01b427f2c8557
tepeszCodes <tomek.szymanderski@gmail.com>
2021-08-18 20:18:22 +0200

    added logger.js with winston config and updated .gitignore to ignore log file

---
+++ src/logger.js
@@ -0,0 +1,58 @@
+import winston from 'winston';
+
+const env = process.env.NODE_ENV || 'development';
+const {
+  combine,
+  timestamp,
+  colorize,
+  printf
+} = winston.format;
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+/** function evaluating which level of logging is to be used.
+ * @returns {String} - a level name
+ */
+const level = env === 'development' ? 'debug' : 'warn';
+const colors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'blue',
+  http: 'green',
+  debug: 'white'
+};
+// Two separate formats need to be created, because colorize() causes reduntant characters
+// to show up in the log file.
+const consoleFormat = combine(
+  timestamp({ format: 'YYY-MM-DD HH:mm:ss:ms' }),
+  colorize({ all: true }),
+  printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
+);
+const fileFormat = combine(
+  timestamp({ format: 'YYY-MM-DD HH:mm:ss:ms' }),
+  printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
+);
+const transports = env === 'production'
+  ? [new winston.transports.File({
+    filename: './logs.log',
+    format: fileFormat,
+  })]
+  : [
+    new winston.transports.Console({ format: consoleFormat }),
+    new winston.transports.File({
+      filename: './logs.log',
+      format: fileFormat,
+    })
+  ];
+winston.addColors(colors);
+const logger = winston.createLogger({
+  level,
+  levels,
+  transports
+});
+
+export default logger;
