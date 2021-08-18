bf7affb4f91bcd1b4251a117960c5c34c3b7b52e
tepeszCodes <tomek.szymanderski@gmail.com>
2021-08-18 21:26:27 +0200

    updated app.js to use morgan middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import morganMiddleware from './loggingMiddleware/morgan';
 
 const app = express();
 
 const PORT = process.env.port || 3000;
 
+app.use(morganMiddleware);
 app.get('/', (req, res) => {
   res.send('This is my first Express server');
 });
