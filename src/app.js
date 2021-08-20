d479684d2916d37b5e6dc993b4a2aff37f4fa651
tepeszCodes <tomek.szymanderski@gmail.com>
2021-08-20 12:13:51 +0200

    moved error catching middleware after app.get

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ import errorHandler from './errorHandler';
 const app = express();
 const { genericError } = errorHandler;
 
+app.use(morganMiddleware);
+app.get('/', (req, res) => {
+  res.send('This is my first Express server');
+});
 app.use('/', (err, req, res, next) => {
   // if response has already started streaming and error occurs, pass it to Express
   // default error handler - it will close the connection and fail the request.
@@ -13,11 +17,6 @@ app.use('/', (err, req, res, next) => {
   } else {
     genericError(err, req, res);
   }
-});
-
-app.use(morganMiddleware);
-app.get('/', (req, res) => {
-  res.send('This is my first Express server');
 });
 
 export default app;
