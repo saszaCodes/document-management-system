9e35b9b10e895c1a5d429317d048ecddbb1be952
tepeszCodes <tomek.szymanderski@gmail.com>
2021-09-17 21:07:30 +0200

    updated knex config to use env variables

--- src/lib/knex/knexfile.js
+++ src/lib/knex/knexfile.js
@@ -1,10 +1,18 @@
+const {
+  KNEX_CLIENT,
+  DB_URL,
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD
+} = process.env;
+
 export default {
-  client: 'pg',
+  client: KNEX_CLIENT,
   connection: {
-    host: 'ella.db.elephantsql.com',
-    user: 'jlzyjhwx',
-    password: 'bggtp4pTzPH3eZHG6L4KbO5HTRZJaf3v',
-    database: 'jlzyjhwx'
+    host: DB_URL,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME
   },
   migrations: {
     tableName: 'knex_migrations',
