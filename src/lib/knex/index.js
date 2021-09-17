1cf3684adf53c90066be938ecf931e36beaabcd8
tepeszCodes <tomek.szymanderski@gmail.com>
2021-09-17 10:39:33 +0200

    (bug fix) fixed knex connection settings

--- src/lib/knex/index.js
+++ src/lib/knex/index.js
@@ -3,7 +3,7 @@ import knexFunc from 'knex';
 const knex = knexFunc({
   client: 'pg',
   connection: {
-    host: 'postgres://jlzyjhwx:bggtp4pTzPH3eZHG6L4KbO5HTRZJaf3v@ella.db.elephantsql.com/jlzyjhwx',
+    host: 'ella.db.elephantsql.com',
     user: 'jlzyjhwx',
     password: 'bggtp4pTzPH3eZHG6L4KbO5HTRZJaf3v',
     database: 'jlzyjhwx'
