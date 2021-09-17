452584e681eca0b73d78b24cf2d5cbdfe3dbdb64
tepeszCodes <tomek.szymanderski@gmail.com>
2021-09-17 10:42:06 +0200

    separated knex setting and instatiation to two files

--- src/lib/knex/index.js
+++ src/lib/knex/index.js
@@ -1,13 +1,4 @@
-import knexFunc from 'knex';
+import knex from 'knex';
+import knexSettings from './knexfile';
 
-const knex = knexFunc({
-  client: 'pg',
-  connection: {
-    host: 'ella.db.elephantsql.com',
-    user: 'jlzyjhwx',
-    password: 'bggtp4pTzPH3eZHG6L4KbO5HTRZJaf3v',
-    database: 'jlzyjhwx'
-  }
-});
-
-export default knex;
+export default knex(knexSettings);
---
+++ src/lib/knex/knexfile.js
@@ -0,0 +1,9 @@
+export default {
+  client: 'pg',
+  connection: {
+    host: 'ella.db.elephantsql.com',
+    user: 'jlzyjhwx',
+    password: 'bggtp4pTzPH3eZHG6L4KbO5HTRZJaf3v',
+    database: 'jlzyjhwx'
+  }
+};
