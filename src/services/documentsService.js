82232a7b8e9d4116eef48d59eae7bfadf0e0f1b4
tepeszCodes <tomek.szymanderski@gmail.com>
2021-10-20 17:18:59 +0200

    updated services handling /documents route

--- src/services/documentsService.js
+++ src/services/documentsService.js
@@ -1,81 +1,122 @@
-import { userProfiles, documents } from '../models';
+/* eslint-disable require-jsdoc */
+import { errorHandlers } from '../middleware';
+import { Documents, UserProfiles } from '../models';
 
-export const documentsService = {
-  createDocument: async (req, res, next) => {
-    const { title, body } = req.body;
-    const authorId = req.body.author_id;
-    // title and authorId are required, if it is not supplied, send 400 Bad Request status
-    if (!title || !authorId) {
-      res.status(400).send('Invalid request. Title and author_id need to be supplied');
-      return;
-    }
-    // if authorId doesn't match existing user,
-    // send 400 Bad Request
-    const validId = await userProfiles
-      .checkIfProfileExists({ id: authorId })
-      .catch((err) => { next(err); });
-    if (!validId) {
-      res.status(400).send(`Invalid request. User with id ${authorId} doesn't exist.`);
-      return;
-    }
-    // if document is successfully created, send its id, title and body
-    const doc = await documents
-      .addDocument(title, body, authorId)
-      .catch((err) => { next(err); });
-    res.status(200).send(doc);
-  },
-  fetchDocument: async (req, res, next) => {
-    const { id } = req.params;
-    // if document doesn't exist, send 404
-    const documentExists = await documents
-      .checkIfDocumentExists({ id })
-      .catch((err) => { next(err); });
-    if (!documentExists) {
-      res.status(404).send('Document not found.');
-      return;
-    }
-    // else, send the document
-    const doc = await documents
-      .getDocumentById(id)
-      .catch((err) => { next(err); });
-    res.status(200).send(doc);
-  },
-  updateDocument: async (req, res, next) => {
-    const { title, body } = req.body;
-    const { id } = req.params;
-    // if no update values are supplied in request,
-    // return 400 Bad Request status
-    if (!title && !body) {
-      res.status(400).send('Invalid request.');
-      return;
-    }
-    // if document doesn't exist, send 404 Not Found status
-    const documentExists = await documents
-      .checkIfDocumentExists({ id })
-      .catch((err) => { next(err); });
-    if (!documentExists) {
-      res.status(404).send('Document doesn\'t exist.');
-      return;
-    }
-    // else, send updated document data back
-    const updatedColumns = await documents
-      .updateDocument(id, { title, body })
-      .catch((err) => { next(err); });
-    res.status(200).send(updatedColumns);
-  },
-  deleteDocument: async (req, res, next) => {
-    const { id } = req.params;
-    // if document doesn't exist, send 404 Not Found status
-    const documentExists = await documents
-      .checkIfDocumentExists({ id })
-      .catch((err) => { next(err); });
-    if (!documentExists) {
-      res.status(404).send('Document doesn\'t exist.');
-      return;
-    }
-    await documents
-      .updateDocument(id, { deleted_at: new Date(Date.now()).toUTCString() })
-      .catch((err) => { next(err); });
-    res.status(200).send('Document successfully deleted.');
+const { generic } = errorHandlers;
+
+class DocumentsService {
+  constructor() {
+    this.documents = new Documents();
+    this.userProfiles = new UserProfiles();
   }
-};
+
+  findDocument = async (req, res, next, conditions) => {
+    const { id } = conditions;
+    if (!id) {
+      return null;
+    }
+    try {
+      const document = await this.documents.read({ id });
+      if (document.length === 0 || (document.length === 1 && document[0].deleted_at)) {
+        return null;
+      }
+      return document[0];
+    } catch (err) {
+      if (res.headersSent) {
+        next(err);
+      } else {
+        generic(err, req, res);
+      }
+    }
+  }
+
+  createDocument = async (req, res, next) => {
+    const { title, body, authorId } = req.body;
+    try {
+      if (!authorId) {
+        res.status(400).send('Author\'s ID is required to create a new document');
+        return;
+      }
+      if (!title && !body) {
+        res.status(400).send('You have to add some title or document body');
+        return;
+      }
+      const user = await this.userProfiles.read({ id: authorId });
+      if (user.length === 0 || (user.length === 1 && user[0].deleted_at)) {
+        res.status(400).send('Wrong author ID, such user doesn\'t exist');
+        return;
+      }
+      const document = await this.documents.create({ title, body, author_id: authorId });
+      res.status(200).send(document[0]);
+    } catch (err) {
+      if (res.headersSent) {
+        next(err);
+      } else {
+        generic(err, req, res);
+      }
+    }
+  }
+
+  fetchDocument = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const document = await this.findDocument(req, res, next, { id });
+      if (document === null) {
+        res.status(404).send('Document not found');
+        return;
+      }
+      res.status(200).send(document);
+    } catch (err) {
+      if (res.headersSent) {
+        next(err);
+      } else {
+        generic(err, req, res);
+      }
+    }
+  }
+
+  updateDocument = async (req, res, next) => {
+    const { title, body } = req.body;
+    const { id } = req.params;
+    try {
+      if (!title && !body) {
+        res.status(400).send('You have to update title or body');
+        return;
+      }
+      const document = await this.findDocument(req, res, next, { id });
+      if (document === null) {
+        res.status(404).send('Document not found');
+        return;
+      }
+      const newDocument = await this.documents.update({ id }, { title, body });
+      res.status(200).send(newDocument[0]);
+    } catch (err) {
+      if (res.headersSent) {
+        next(err);
+      } else {
+        generic(err, req, res);
+      }
+    }
+  }
+
+  deleteDocument = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const document = await this.findDocument(req, res, next, { id });
+      if (document === null) {
+        res.status(404).send('Document not found');
+        return;
+      }
+      await this.documents.update({ id }, { deleted_at: new Date(Date.now()).toUTCString() });
+      res.status(200).send('Document deleted');
+    } catch (err) {
+      if (res.headersSent) {
+        next(err);
+      } else {
+        generic(err, req, res);
+      }
+    }
+  }
+}
+
+export default new DocumentsService();
--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,4 @@
-import { documentsService } from './documentsService';
+import documentsService from './documentsService';
 import usersService from './usersService';
 
 export { usersService, documentsService };
