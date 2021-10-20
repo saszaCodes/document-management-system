12fa278c11ce23c7e9dfcc9d53f8354e876678f6
tepeszCodes <tomek.szymanderski@gmail.com>
2021-10-20 17:01:02 +0200

    removed UNIQUE constraints from username and email columns

--- src/lib/knex/migrations/20210917093646_user_logins.js
+++ src/lib/knex/migrations/20210917093646_user_logins.js
@@ -2,7 +2,7 @@ export function up(knex) {
   return knex.schema
     .createTable('user_logins', (table) => {
       table.increments('id');
-      table.string('username').notNullable().unique();
+      table.string('username').notNullable();
       table.string('password').notNullable();
       table.integer('user_profile_id').notNullable().references('id').inTable('user_profiles');
       table.date('last_login');
