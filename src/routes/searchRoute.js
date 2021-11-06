8f524d7cf443aab09e48e8193a9e842e20dc68fc
tepeszCodes <tomek.szymanderski@gmail.com>
2021-11-06 15:03:32 +0100

    added service and routing for /search/documents endpoint

--- src/routes/searchRoute.js
+++ src/routes/searchRoute.js
@@ -6,6 +6,7 @@ const searchRouter = express.Router();
 const { generic } = errorHandlers;
 
 searchRouter.get('/search/users', searchService.searchForUsers);
+searchRouter.get('/search/documents', searchService.searchForDocs);
 searchRouter.use('/search', (err, req, res, next) => {
   // if response has already started streaming and error occurs, pass it to Express
   // default error handler - it will close the connection and fail the request.
--- src/services/searchService.js
+++ src/services/searchService.js
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
 import { errorHandlers } from '../middleware';
-import { UserProfiles, UserLogins } from '../models';
+import { UserProfiles, UserLogins, Documents } from '../models';
 
 const { generic } = errorHandlers;
 
@@ -10,9 +10,10 @@ class SearchService {
   constructor() {
     this.userProfiles = new UserProfiles();
     this.userLogins = new UserLogins();
+    this.documents = new Documents();
   }
 
-  /** Helper method. Finds a user matching given conditions
+  /** Finds users matching given query (expected query params: q, searchBy)
    * @param {Object} req - request object
    * @param {Object} res - response object
    * @param {Function} next - passes errors to next Express middleware
@@ -43,6 +44,38 @@ class SearchService {
       }
       // else, send 200 OK with users' data
       res.status(200).send(users);
+    } catch (err) {
+      if (res.headersSent) {
+        next(err);
+      } else {
+        generic(err, req, res);
+      }
+    }
+  }
+
+  /** Finds documents matching given query (expected query param: q)
+   * @param {Object} req - request object
+   * @param {Object} res - response object
+   * @param {Function} next - passes errors to next Express middleware
+   * @returns {undefined}
+   */
+  searchForDocs = async (req, res, next) => {
+    try {
+      // validate the request
+      const { q } = req.query;
+      if (!q) {
+        res.status(400).send('You must provide query parameters. If you want to fetch all users, send GET to /users endpoint');
+        return;
+      }
+      const conditionsArr = ['title', 'like', `%${q}%`];
+      const docs = await this.documents.read(null, conditionsArr);
+      // if no documents are found, send 200 OK with appropriate info
+      if (docs.length === 0) {
+        res.status(200).send('No users matching your criteria were found');
+        return;
+      }
+      // else, send 200 OK with documents' data
+      res.status(200).send(docs);
     } catch (err) {
       if (res.headersSent) {
         next(err);
